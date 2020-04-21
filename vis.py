efb15620fcb0fe6d08c6d48c17b80b026f7321c4
Izzy Brand <izzybrand@gmail.com>
2020-04-21 17:24:20 -0400

    changed animation to take a trajectory

--- vis.py
+++ vis.py
@@ -2,26 +2,29 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 
+from simple_kite_dynamics import Kite
+# TODO: this shouldn't be in the global scope. wrap in a class
+kite = Kite()
+
 def setup_ax(ax):
     ax.set_xlabel('X (m)')
     ax.set_ylabel('Y (m)')
     ax.set_zlabel('Z (m)')
 
     # TODO: set aspect ratio
-    # TODO: this should be related to the max tether length 
-    ax.set_xlim(-5, 60)  
+    # TODO: this should be related to the max tether length
+    ax.set_xlim(-5, 60)
     ax.set_ylim(-60, 60)
     ax.set_zlim(-5, 60)
 
-def plot(ax, kite, u, w):
+def plot(ax, x, u, w):
     # get the kite state
-    x = kite.state
     # position of the origin
     o = np.zeros(3)
 
     # draw a line from the origin to the center of the kite
     tether = np.array([o,kite.p(x)])
-    ax.plot3D(*tether.T, 'gray')
+    ax.plot3D(*tether.T, 'gray', alpha=0.5)
 
     # draw a line at the kite center  in the direction of e_t
     e_t = np.array([o, 10 * kite.e_t(x, u, w)]) + kite.p(x)
@@ -33,45 +36,60 @@ def plot(ax, kite, u, w):
 
     # plot the axes
     for v, c in zip(kite.get_string_basis(x), ['red', 'green', 'blue']):
-        ax.plot3D(*np.array([o,10*v]).T, color=c, alpha=0.5)
+        ax.plot3D(*np.array([o,20 * v]).T, color=c)
 
     # draw a dot at the origin
     ax.scatter3D(*o)
-    plt.legend()
 
-def update(num, ax, kite, U, w):
+def update(t, ax, X, U, W):
     # get the command
-    u = U[num]
-    # dynamics update
-    print(kite.p(kite.state))
-    kite.state += 0.05 * kite.f(kite.state, u, w)
+    x = X[t]
+    w = W[t]
+    u = U[t]
     # plot the kite
     ax.cla()
     setup_ax(ax)
-    plot(ax, kite, u, w)
+    plot(ax, x, u, w)
+    ax.legend()
+    ax.set_title('t = {:8.4f}"'.format(t*dt))
 
-if __name__ == '__main__':
-    theta = np.pi/4
-    phi = np.pi/4
-    thetadot = 0
-    phidot = 0
+def animate_trajectory(X, U, W):
+    T = X.shape[0]
 
-    x_0 = np.array([theta, phi, thetadot, phidot])
-    w = np.array([6, 0, 0])
-    u = -np.pi/8
-    T = 100 # how many frames
-    U = [u]*T # trajectory of controls
+    assert U.shape[0] == T
+    assert W.shape[0] == T
 
-    from simple_kite_dynamics import Kite
-    kite = Kite()
-    kite.state = x_0
-
-    
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     setup_ax(ax)
 
-    line_ani = animation.FuncAnimation(fig, update, frames=T, fargs=(ax, kite, U, w),
+    line_ani = animation.FuncAnimation(fig, update, frames=T, fargs=(ax, X, U, W),
                                        interval=33, blit=False)
 
     plt.show()
+
+
+if __name__ == '__main__':
+    T = 100 # how many frames
+    dt = 0.05
+
+    # trajectectory of wind vectors
+    W = np.ones([T,3]) * np.array([6, 0, 0])[None,:]
+    # trajectory of control inputs
+    U = np.ones(T) * -np.pi/8
+
+
+    theta = np.pi/4
+    phi = np.pi/4
+    thetadot = 0
+    phidot = 0
+    x_0 = np.array([theta, phi, thetadot, phidot])
+
+    # simulate the kite in time
+    X = [x_0]
+    for t in range(T-1):
+        dx = kite.f(X[t], U[t], W[t])
+        X.append(X[t] + dx*dt)
+    X = np.array(X)
+
+    animate_trajectory(X, U, W)
