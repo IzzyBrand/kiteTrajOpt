c8be871a0cda8d509e77eb64f9c1c2f282ba7053
Izzy Brand <izzybrand@gmail.com>
2020-04-29 16:28:02 -0400

    added rdot to state in dynamics

--- simple_kite_dynamics.py
+++ simple_kite_dynamics.py
@@ -2,7 +2,6 @@ import numpy as np
 
 class Kite:
     def __init__(self):
-        self.r = 50.    # length of the cable in meters
         self.m = 1      # kg
         self.g = 9.81   # m/s^2
         self.rho = 1.2  # air density
@@ -13,31 +12,31 @@ class Kite:
     def p(self, x):
         """ get the kite position in global frame
         """
-        theta, phi, _, _ = x
+        theta, phi, r _, _, _ = x
 
         s_t = np.sin(theta)
         c_t = np.cos(theta)
         s_p = np.sin(phi)
         c_p = np.cos(phi)
 
-        x = self.r*s_t*c_p
-        y = self.r*s_t*s_p
-        z = self.r*c_t
+        x = r*s_t*c_p
+        y = r*s_t*s_p
+        z = r*c_t
         return np.array([x,y,z])
 
     def pdot(self, x):
         """ get the kite velocity in global frame
         """
-        theta, phi, thetadot, phidot = x
+        theta, phi, r, thetadot, phidot, rdot = x
 
         s_t = np.sin(theta)
         c_t = np.cos(theta)
         s_p = np.sin(phi)
         c_p = np.cos(phi)
 
-        xdot = self.r*(c_t*c_p*thetadot - s_t*s_p*phidot)
-        ydot = self.r*(c_t*s_p*thetadot + s_t*c_p*phidot)
-        zdot = self.r*(-s_t*thetadot)
+        xdot = r*(c_t*c_p*thetadot - s_t*s_p*phidot) + rdot*s_t*c_p
+        ydot = r*(c_t*s_p*thetadot + s_t*c_p*phidot) + rdot*s_t*s_p
+        zdot = r*(-s_t*thetadot) + rdot*c_t
 
         return np.array([xdot, ydot, zdot])
 
@@ -78,7 +77,7 @@ class Kite:
     def get_string_basis(self, x):
         """ get a coordinate frame rotated by the kite's position
         """
-        theta, phi, _, _ = x
+        theta, phi, _, _, _, _ = x
         s_t = np.sin(theta)
         c_t = np.cos(theta)
         s_p = np.sin(phi)
@@ -137,13 +136,13 @@ class Kite:
     def f(self, x, u, w=np.array([6,0,0])):
         """ dynamics of the kite
         """
-        theta, phi, thetadot, phidot = x
+        theta, phi, r, thetadot, phidot, rdot = x
     
-        thetadotdot = self.F_theta_aer(x, u, w)/(self.r*self.m) +\
-            np.sin(theta)*self.g/self.r +\
+        thetadotdot = self.F_theta_aer(x, u, w)/(r*self.m) +\
+            np.sin(theta)*self.g/r +\
             np.sin(theta)*np.cos(theta)*phidot**2
 
-        phidotdot = self.F_phi_aer(x, u, w)/(self.r*self.m) -\
+        phidotdot = self.F_phi_aer(x, u, w)/(r*self.m) -\
             2/np.tan(theta)*phidot*thetadot
 
         xdot = np.array([thetadot, phidot, thetadotdot, phidotdot])
